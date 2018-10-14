c5701713b02381b1b7ef209041ec21b1e43bb169
Stephen Tvedt <stvedt@zola.com>
2018-10-13 23:11:05 -0400

    rough start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MomentReact from 'react-moment';
+import moment from 'moment-timezone';
+import IntegrationReactSelect from './SearchBox';
 
 class App extends Component {
+  // constructor(props){
+  //   super(props);
+  // }
+  calcDay(time) {
+    let dayTimes = [];
+    for(let i = 0; i < 24; i++){
+      dayTimes.push(
+        <MomentReact
+          className="time"
+          add={{ hours: i }}
+          format='hh:mm a'
+          key={time+i}>
+            {time}
+        </MomentReact>
+      );
+    }
+    return dayTimes;
+  }
+
   render() {
+    const now = moment();
+    const startDay = now.startOf('day')
+    const india = moment(startDay).tz('Asia/Kolkata')
+    // console.log(moment.tz.names());
+
+    let nyTimes = this.calcDay(now);
+    let indiaRows = this.calcDay(india);
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="left" key="left">
+          <IntegrationReactSelect onChange={(value) => console.log(value)}/>
+          <h3>NYC</h3>
+          {nyTimes}
+        </div>
+        <div className="right" key="right">
+          <IntegrationReactSelect onChange={(value) => console.log(value)}/>
+          <h3>India</h3>
+          {indiaRows}
+        </div>
       </div>
     );
   }
